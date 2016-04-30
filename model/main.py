49f667c7fc8dd086e77f242505a134d4651777b4
Alexandr Azizyan <alex.r.azizian@gmail.com>
2016-04-30 16:04:49 +0400

    Add new route to return json data

--- model/main.py
+++ model/main.py
@@ -5,7 +5,7 @@ import route
 app = Flask(__name__)
 
 app.register_blueprint(route.routing)
-
+app.register_blueprint(route.datarouting)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
--- model/route.py
+++ model/route.py
@@ -9,3 +9,16 @@ routing = Blueprint('route', __name__,
 def route_answer():
     _dict = {'HELLO': 'WORLD'}
     return flask.jsonify(**_dict)
+
+
+datarouting = Blueprint('data', __name__, url_prefix='/data')
+
+
+@datarouting.route('/json')
+def json_data():
+    try:
+        with open('data1.json') as json_file:
+            return json_file.read(), 200
+    except EnvironmentError as e:
+        print(e)
+        return flask.jsonify(result={'status': 500})
