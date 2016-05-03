abdf52bebc415f9e9a185204a5e42c84a2966868
misho1995 <mnems13@freeuni.edu.ge>
2016-05-03 13:06:13 +0400

    Improve reducing.

--- model/reducer/reducer.py
+++ model/reducer/reducer.py
@@ -22,23 +22,29 @@ class Reducer:
     def reduce_data(self):
         data_source = DataSource.get_instance()
         handler = data_source.read_from_file(100000)
-        grid = {}
         start_c = (GEO_LD_X, GEO_LD_Y)
         end_c = (GEO_RU_X, GEO_RU_Y)
+        self.reduce_entries(start_c, end_c, handler, data_source)
+
+    def reduce_entries(self, start_c, end_c, entries, dt_src):
+        grid = {}
         size = (GRID_SIZE_X, GRID_SIZE_Y)
         translator = Translator(start_c, end_c, size)
-        for entry in handler:
+        for entry in entries:
             x = entry.lon
             y = entry.lat
             x, y = translator.translate_to_grid((x, y))
             count = get_from_grid(grid, x, y)
             set_to_grid(grid, x, y, count + 1)
+        self.reduce_map(grid, size, translator, dt_src)
+
+    def reduce_map(self, grid, size, translator, dt_src):
         grid = self._avg_for_each(grid, size)
         average = self._get_grid_avg(grid)
         min_val = self._remove_trashold(grid, average)
         grid = self._scale(grid, min_val)
         coordinate_list = self._generate_list(grid, translator)
-        data_source.set_entries(coordinate_list)
+        dt_src.set_entries(coordinate_list)
         # print(coordinate_list)
         # print(len(coordinate_list))
 
@@ -59,7 +65,7 @@ class Reducer:
         average = 0
         for _, val in grid.items():
             average = max(average, val) # (average + val) / 2
-        return int(math.sqrt(average))
+        return int(math.sqrt(int(math.sqrt(average))))
 
     def _remove_trashold(self, grid, average):
         _min = 100000000000
@@ -74,7 +80,9 @@ class Reducer:
         for key, val in grid.items():
             old_val = get_from_grid(grid, key[0], key[1])
             if old_val > min_val:
-                new_val =  old_val - min_val + 1
+                diff = int(old_val / min_val)
+                # new_val = old_val - min_val * diff + 1
+                new_val = diff
                 set_to_grid(grid, key[0], key[1], new_val)
         return grid
 
